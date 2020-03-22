5598c91a49b58767db7f80dd87932b4430dfd118
hyeseung.lee <hyeseung.lee@lge.com>
2020-03-22 19:20:52 +0900

    upload chap10.4 ID:STRJOIN code

---
+++ chap10/hs/STRJOIN.cpp
@@ -0,0 +1,71 @@
+// 한 개의 문자열로 합칠 때 필요한 최소 비용
+// --> 입력된 문자열 길이를 오름차순 정렬하고 작은거부터 합치면 됨!
+// 참조 : https://hyeonstorage.tistory.com/324 (vector에 관해)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void printVector(const vector<int> &a)
+{
+    for(int i=0; i<a.size(); i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int calcMinPrice(vector<int> &strLen)
+{
+    if(strLen.size() <= 1) return 0;
+
+    int minFirst = strLen.front();
+    strLen.erase(strLen.begin()); // 제일 작은거 vector에서 삭제
+                                  // (+ 뒷 요소들이 자동으로 한 칸 씩 앞으로 당겨짐)
+    int minSecond = strLen.front();
+    strLen.erase(strLen.begin()); // 두 번째 작은거 vector에서 삭제
+
+    int sum = minFirst + minSecond;
+
+    strLen.push_back(sum);
+    sort(strLen.begin(), strLen.end());
+
+    //printVector(strLen);
+
+    return sum + calcMinPrice(strLen);
+}
+
+int main()
+{
+    int testCase;
+    cin >> testCase;
+
+    if(testCase < 1 || testCase > 50)
+        exit(-1);
+
+    for(int i=0; i<testCase; i++)
+    {
+        // n : 문자열의 수 (1<=n<=100)
+        int n;
+        cin >> n;
+        if(n < 1 || n > 100)
+            exit(-1);
+
+        // n개 만큼 vector 생성!
+        // strLen : 문자열의 길이 (1<=strLen<=1000)
+        vector<int> strLen(n);
+        for(int j=0; j<n; j++)
+        {
+            cin >> strLen[j];
+            if(strLen[j] < 1 || strLen[j] > 1000)
+                exit(-1);
+        }
+
+        sort(strLen.begin(), strLen.end()); // 오름차순 정렬
+        cout << calcMinPrice(strLen) << endl;
+    }
+
+    return 0;
+}
